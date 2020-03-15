2b39fe998fb52c08a36aabfc232de28c87477ec4
Malene <maleneklit@hotmail.com>
2020-03-15 18:56:02 +0100

    work with vuex

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,11 +1,42 @@
-
 <template>
   <div>
-    <h1>{{ event.title }}</h1>
+    <div class="event-header">
+      <span class="eyebrow">
+        @{{ event.time }} on {{ event.date }}
+      </span>
+      <h1 class="title">
+        {{ event.title }}
+      </h1>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+    
+    <span name="map">
+      <h2>Location</h2>
+    </span>
+    
+    <address>{{ event.location }}</address>
+    
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+    
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">
+        {{ event.attendees ? event.attendees.length : 0 }}
+      </span>
+    </h2>
+    <ul class="list-group">
+      <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   head() {
     return {
@@ -19,26 +50,98 @@ export default {
       ]
     }
   },
-  async asyncData({ $axios, error, params }) {
+  async fetch({ store, params, error }) {
     try {
-      // here we use es6 desctrutoring - it could also just be const response
-      const { data } = await $axios.get(
-        'http://localhost:3000/events/' + params.id
-      )
-      return {
-        // this merge with the componet data!! and events can just get used in the template
-        event: data
-      }
+      await store.dispatch('events/fetchEvent', params.id)
     } catch (e) {
       error({
-        // this renders the error page with the message if we have a error
         statusCode: 503,
-        message: 'Unable to fetch event #' + params.id + '. Please try again.'
+        message: 'Unable to fetch event #' + params.id
       })
     }
-  }
+  },
+  computed: mapState({
+    event: state => state.events.event
+  })
 }
+
+// Does not use Vuex
+// import EventService from '@/services/EventService.js'
+
+// export default {
+//   head() {
+//     return {
+//       title: 'Event: ' + this.event.title,
+//       meta: [
+//         {
+//           hid: 'description',
+//           name: 'description',
+//           content: 'What you need to know about Event ' + this.event.title
+//         }
+//       ]
+//     }
+//   },
+//   // async asyncData({ $axios, error, params }) {
+//   // when using the Eventservice we can remove $axios
+//   async asyncData({ error, params }) {
+//     try {
+//       // here we use es6 desctrutoring - it could also just be const response
+//       const { data } = await EventService.getEvent(params.id)
+//       return {
+//         // this merge with the componet data!! and events can just get used in the template
+//         event: data
+//       }
+//     } catch (e) {
+//       error({
+//         // this renders the error page with the message if we have a error
+//         statusCode: 503,
+//         message: 'Unable to fetch event #' + params.id + '. Please try again.'
+//       })
+//     }
+//   }
+// }
 </script>
 
 <style scoped>
+.prompt-box {
+  position: relative;
+  overflow: hidden;
+  padding: 1em;
+  margin-bottom: 24px;
+  transform: scaleY(1);
+}
+.prompt-box > .title {
+  margin: 0 0 0.5em;
+}
+.prompt-box > .title > .meta {
+  margin-left: 10px;
+}
+.prompt-box > .actions {
+  display: flex;
+  align-items: center;
+}
+.prompt-box > button {
+  margin-right: 0.5em;
+}
+.prompt-box > button:last-of-type {
+  margin-right: 0;
+}
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,40 +12,16 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
 export default {
   head() {
     return {
       title: 'Event Listing' // Title for the page
     }
   },
-  // asyncData(context) {
-  // return context.$axios.get('http://localhost:3000/events').then(response => {
-  // or use es6 destructoring
-  // asyncData({ $axios, error }) {
-  //   return $axios
-  //     .get('http://localhost:3000/events')
-  //     .then(response => {
-  //       return {
-  //         // this merge with the componet data!! and events can just get used in the template
-  //         events: response.data
-  //       }
-  //     })
-  //     .catch(e => {
-  //       error({
-  //         // this renders the error page with the message if we have a error
-  //         statusCode: 503,
-  //         message: 'Unable to fetch events at this time. Please try again.'
-  //       })
-  //     })
-  // },
-  async asyncData({ $axios, error }) {
+  async fetch({ store, error }) {
     try {
-      // here we use es6 desctrutoring - it could also just be const response
-      const { data } = await $axios.get('http://localhost:3000/events')
-      return {
-        // this merge with the componet data!! and events can just get used in the template
-        events: data
-      }
+      await store.dispatch('events/fetchEvents')
     } catch (e) {
       error({
         // this renders the error page with the message if we have a error
@@ -56,6 +32,64 @@ export default {
   },
   components: {
     EventCard
-  }
+  },
+  computed: mapState({
+    events: state => state.events.events
+  })
 }
+
+// import EventCard from '@/components/EventCard.vue'
+// import EventService from '@/services/EventService.js'
+
+// How to make API calls withouth vuex
+// export default {
+//   head() {
+//     return {
+//       title: 'Event Listing' // Title for the page
+//     }
+//   },
+//   // asyncData(context) {
+//   // return context.$axios.get('http://localhost:3000/events').then(response => {
+//   // or use es6 destructoring
+//   // asyncData({ $axios, error }) {
+//   //   return $axios
+//   //     .get('http://localhost:3000/events')
+//   //     .then(response => {
+//   //       return {
+//   //         // this merge with the componet data!! and events can just get used in the template
+//   //         events: response.data
+//   //       }
+//   //     })
+//   //     .catch(e => {
+//   //       error({
+//   //         // this renders the error page with the message if we have a error
+//   //         statusCode: 503,
+//   //         message: 'Unable to fetch events at this time. Please try again.'
+//   //       })
+//   //     })
+//   // },
+//   // async asyncData({ $axios, error }) {
+//   // when using the Eventservice we can remove $axios
+//   async asyncData({ error }) {
+//     try {
+//       // here we use es6 desctrutoring - it could also just be const response
+//       // const { data } = await $axios.get('http://localhost:3000/events')
+//       // Now using Vuex
+//       const { data } = await EventService.getEvents()
+//       return {
+//         // this merge with the componet data!! and events can just get used in the template
+//         events: data
+//       }
+//     } catch (e) {
+//       error({
+//         // this renders the error page with the message if we have a error
+//         statusCode: 503,
+//         message: 'Unable to fetch events at this time. Please try again.'
+//       })
+//     }
+//   },
+//   components: {
+//     EventCard
+//   }
+// }
 </script>
